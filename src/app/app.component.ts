28b833bffb6c8e40f6d79f0fdb3a51127710ff3e
Marlou Stolk <marloustolk@marlous-macbook-air.home>
2024-03-09 21:43:29 +0100

    more primeng less css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, SidebarModule, ProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  sidebarVisible: boolean = false;
   constructor(private title: Title) { }
 
   ngOnInit() {
